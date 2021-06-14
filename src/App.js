b116102d28cd04e84f428b63352b8a2e63367f11
Jeremiah <jeremiah.r29q7.tabb@gmail.com>
2021-06-14 13:10:01 -0400

    First add to Netlify

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Route, Switch } from "react-router-dom";
+import QualityGraph from "./components/QualityGraph/QualityGraph";
+import QuantityGraph from "./components/QuantityGraph/QuantityGraph";
+import Home from "./components/Home/Home";
+import NavBar from "./components/NavBar/NavBar";
+
+const Api =
+"https://glorify-the-supreme-god-67d35a.herokuapp.com";
+// "http://localhost:4000";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [qualityData, setQualityData] = useState(null);
+  const [quantityData, setQuantityData] = useState(null);
+
+  useEffect( () => {
+
+    fetch(
+      `${Api}/${'qualityData'}`
+    )
+    .then((r) => r.json())
+    .then((da)=>{
+      setQualityData(
+        da.sort((a,b) => Date.parse(b.ts) - Date.parse(a.ts))
+      );
+    });
+
+    fetch(
+      `${Api}/${'quantityData'}`
+    )
+    .then((r) => r.json())
+    .then((da)=>{
+      setQuantityData(
+        da.sort((a,b) => Date.parse(b.ts) - Date.parse(a.ts))
+      );
+    });
+  }, []);
+
+  return qualityData !== null ?  (
+    <div>
+      <NavBar />
+
+        <Switch>
+
+          <Route exact path="/quality">
+            <QualityGraph qualityData={qualityData} setQualityData={setQualityData}
+            />
+          </Route>
+
+          <Route exact path="/quantity">
+            <QuantityGraph quantityData={quantityData} setQuantityData={setQuantityData}
+            />
+          </Route>
+
+          <Route exact path="/">
+            <Home qualityData={qualityData} setQualityData={setQualityData}
+            />
+          </Route>
+
+        </Switch> 
     </div>
+  ) : (
+    <p>loading ... </p> 
   );
 }
 
