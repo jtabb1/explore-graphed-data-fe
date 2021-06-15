578b6eff1c6d5fbfc2406e6deec3eea7c34cdb72
Jeremiah <jeremiah.r29q7.tabb@gmail.com>
2021-06-15 10:52:19 -0400

    Make iPhone friendly front page.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,12 +3,28 @@ import { Link } from 'react-router-dom';
 import "./Home.css";
 
 function Home() {
-    return (
-      <div className="container m-4">
-        <h1>Explore Your Data:</h1>
-        <h3>Choose either <Link to="/quality">Quality</Link>, or <Link to="/quantity">Quantity</Link></h3>.
-      </div>
-    )
-  }
-  
-  export default Home;
+
+  // let g01aWidthScaling = "0.9";
+  // let g01aHeightScaling = "0.4";
+  // let vptW = window.innerWidth;
+  // let vptH = window.innerHeight;
+  // let g01aW = +g01aWidthScaling * vptW;
+  // let g01aH = +g01aHeightScaling * vptH;
+
+  return (
+    <div className="container m-5" width="100px">
+      <h1 width="300px">Data To Go:</h1>
+      <p width="300px">
+        You now have the ability to view, edit, and populate important 
+        business metrics on the go.
+      </p>
+      <p width="300px">
+        Choose which metric to get started with:
+      </p>
+      <h3><Link to="/quality">Quality</Link></h3>.
+      <h3><Link to="/quantity">Quantity</Link></h3>.
+    </div>
+  )
+}
+
+export default Home;
